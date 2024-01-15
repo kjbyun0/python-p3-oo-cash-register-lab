f416d03f0ca2df2d2a13dd0fcbfa010d5b7caf22
KI JONG BYUN <kjbyun0@gmail.com>
2024-01-15 05:39:01 -0600

    Assignment Done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.last_count = 0
+    self.last_total = 0
+
+  def add_item(self, name, price, count=1):
+    for i in range(count):
+      self.items.append(name)
+    self.last_count = count
+    self.last_total = price * count
+    self.total += self.last_total
+
+
+  def apply_discount(self):
+    if self.discount != 0:
+      self.total -= self.total * (self.discount / 100)
+      self.last_total -= self.last_total * (self.discount / 100)
+      print(f'After the discount, the total comes to ${int(self.total)}.')
+    else:
+      print('There is no discount to apply.')
+
+  def reset_register_totals(self):
+    self.total = 0
+    self.items = []
+    self.last_count = 0
+    self.last_total = 0
+
+  def void_last_transaction(self):
+    self.total -= self.last_total
+    for i in range(self.last_count):
+      self.items.pop()
+
+  
